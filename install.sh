4f89dc8f2e60e6df50b628513c4bec68aa6dc204
hubenchang0515 <hubenchang0515@outlook.com>
2018-11-13 21:08:24 +0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+mkdir -p /usr/local/include/luaMagic
+cp luaMagic.hpp /usr/local/include/luaMagic/luaMagic.hpp
+cp luaMagicRead.hpp /usr/local/include/luaMagic/luaMagicRead.hpp
+cp luaMagicWrite.hpp /usr/local/include/luaMagic/luaMagicWrite.hpp
